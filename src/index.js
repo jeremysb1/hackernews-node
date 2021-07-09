9deb3fcae9cf775685b011e73e7ed7c65837916e
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-07-09 15:39:27 -0400

    implement the resolver function

--- src/index.js
+++ src/index.js
@@ -8,17 +8,25 @@ let links = [{
   description: 'Fullstack tutorial for GraphQL'
 }]
 
+let idCOunt = links.length
 const resolvers = {
   Query: {
   	info: () => `This is the API of a Hacker News Clone`,
   	feed: () => links,
   },
 
-  Link: {
-  	id: (parent) => parent.id,
-  	description: (parent) => parent.description,
-  	url: (parent) => parent.url
-  }
+  Mutation: {
+  	post: (parent, args) => {
+  		const link = {
+  		  id: `link-${idCount++}`,
+  		  description: args.description,
+  		  url: args.url,
+  		}
+
+  		links.push(link)
+  		return link
+  	}
+  },
 }
 
 const server = new ApolloServer({
