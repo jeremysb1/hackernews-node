f16611cd901496f355f53b572de1d2a3c5d75188
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-07-15 10:42:52 -0400

    adding full crud functionality to api

--- src/index.js
+++ src/index.js
@@ -11,23 +11,36 @@ let links = [{
 let idCount = links.length
 const resolvers = {
   Query: {
-  	info: () => `This is the API of a Hacker News Clone`,
-  	feed: () => links,
+    info: () => `This is the API of Hackernews Clone`,
+    feed: () => links,
+    link: (parent, args) => links.find((link) => link.id === args.id),
   },
-
   Mutation: {
-  	post: (parent, args) => {
-  		const link = {
-  		  id: `link-${idCount++}`,
-  		  description: args.description,
-  		  url: args.url,
-  		}
-
-  		links.push(link)
-  		return link
-  	}
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+    updateLink: (parent, args) => {
+      const link = links.find((link) => link.id === args.id);
+      link.url = args.url ? args.url : link.url;
+      if (args.description) {
+        link.description = args.description;
+      }
+      return link;
+    },
+    deleteLink: (parent, args) => {
+      const index = links.findIndex((link) => link.id === args.id);
+      const link = links[index];
+      links.splice(index, 1);
+      return link;
+    },
   },
-}
+};
 
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(
