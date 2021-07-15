7abd75e2253d42d554d1585d7b298836729dfee4
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-07-15 12:33:36 -0400

    adding authentiaction resolvers

---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,42 @@
+async function signup(parent, args, context, info) {
+  // encrypt the User’s password using the bcryptjs library
+  const password = await bcrypt.hash(args.password, 10)
+
+  // use PrismaClient instance to store the new User record in the database
+  const user = await context.prisma.user.create({ data: { ...args, password } })
+
+  // generating a JSON Web Token
+  const token = jwt.sign({ userId: user.id }, APP_SECRET)
+
+  // return the token and the user in an object that adheres to the shape of an AuthPayload object
+  return {
+  	token,
+  	user,
+  }
+}
+
+async function login(parent, args, context, info) {
+  // using PrismaClient instance to retrieve an existing User record by email
+  const user = await context.prisma.user.findUnique({ where: { email: args.email } })
+  if (!user) {
+  	throw new Error('No such user found')
+  }
+
+  // compare the provided password with the one that is stored in the database
+  const valid = await bcrypt.compare(args.password, user.password) {
+  	if(!valid) {
+  	  throw new Error('Invalid Password')
+  	}
+  }
+
+  return {
+  	token,
+  	user,
+  }
+}
+
+module.exports = {
+  signup,
+  login,
+  post,
+}
