de1dbec46c8194fbf387197c6c1d510d9b5a0a77
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-07-15 10:14:40 -0400

    added comments

--- src/script.js
+++ src/script.js
@@ -1,24 +1,25 @@
+// 1 Import the PrismaClient constructor from the @prisma/client node module
+
 const { PrismaClient } = require("@prisma/client")
+
+// 2 Instantiate PrismaClient.
 
 const prisma = new PrismaClient()
 
+//3 Define an async function called main to send queries to the database.
+
 async function main() {
-  const newLink = await prisma.link.create({
-    data: {
-      description: 'Fullstack tutorial for GraphQL',
-      url: 'www.howtographql.com',
-    },
-  })
-  
   const allLinks = await prisma.link.findMany()
   console.log(allLinks)
 }
 
+//4 Call the main function.
+
 main()
-  .catch(error => {
-    throw error
+  .catch(e => {
+    throw e
   })
-  // 5
+  // 5 Close database connections when the script terminates.
   .finally(async () => {
     await prisma.$disconnect()
   })
